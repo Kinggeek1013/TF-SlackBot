ecdfb31d6c096d373bf52bb4788575cc65002d29
Kinggeek1013 <47408708+Kinggeek1013@users.noreply.github.com>
2022-05-25 12:04:05 -0600

    Changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 terraform {
     required_version = ">= 0.12"
     backend "s3" {
-        bucket = "YOUR_BUCKET_NAME"
+        bucket = "s3_bucket"
         key = "state"
     }
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,5 @@
-/* get region from AWS_DEFAULT_REGION */
 data "aws_region" "current" {}
+
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
